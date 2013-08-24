5e935b3c5aba73f507a6370d36dedb3af2654f28
Scott Lillibridge <slillibridge@whitepages.com>
2013-08-24 14:44:11 -0700

    Resolve FC023

--- recipes/netscaler.rb
+++ recipes/netscaler.rb
@@ -5,16 +5,14 @@ include_recipe 'diamond::default'
 #load databag
 databag = Chef::EncryptedDataBagItem.load("passwords", "snmp")
 
-if node.default['diamond']['snmp'] == "true"
-  collector_config "NetscalerSNMPCollector" do
-    path      node.default['diamond']['collectors']['NetscalerSNMPCollector']['path']
-    snmp      true
-    interval  node.default['diamond']['collectors']['NetscalerSNMPCollector']['interval']
-    timeout   node.default['diamond']['collectors']['NetscalerSNMPCollector']['timeout']
-    retries   node.default['diamond']['collectors']['NetscalerSNMPCollector']['retries']
-    port      node.default['diamond']['collectors']['NetscalerSNMPCollector']['port']
-    community databag['community']
-    devices   node.default['diamond']['collectors']['NetscalerSNMPCollector']['devices']
-  end 
+collector_config "NetscalerSNMPCollector" do
+path      node.default['diamond']['collectors']['NetscalerSNMPCollector']['path']
+snmp      true
+interval  node.default['diamond']['collectors']['NetscalerSNMPCollector']['interval']
+timeout   node.default['diamond']['collectors']['NetscalerSNMPCollector']['timeout']
+retries   node.default['diamond']['collectors']['NetscalerSNMPCollector']['retries']
+port      node.default['diamond']['collectors']['NetscalerSNMPCollector']['port']
+community databag['community']
+devices   node.default['diamond']['collectors']['NetscalerSNMPCollector']['devices']
+only_if node.default['diamond']['snmp'] == "true"
 end 
-
