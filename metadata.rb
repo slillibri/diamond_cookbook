2660cc53ca60280c3af481440cbcff36b4ebfc30
Scott Lillibridge <slillibridge@whitepages.com>
2013-08-24 14:47:54 -0700

    Version bump for foodcritic tests

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email "cbarraford@brightcove.com"
 license          "All rights reserved"
 description      "Installs/Configures diamond"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "1.0.18"
+version          "1.0.19"
 
 supports         'ubuntu'
 supports         'centos'
