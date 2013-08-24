38b0cab5340430b7ea6067a06e7980ec4402a6c0
Scott Lillibridge <slillibridge@whitepages.com>
2013-08-24 14:19:15 -0700

    Add interval to network collector

--- recipes/network.rb
+++ recipes/network.rb
@@ -5,5 +5,8 @@ include_recipe 'diamond::default'
 collector_config "NetworkCollector" do
   interfaces node[:diamond][:collectors][:NetworkCollector][:interfaces]
   byte_unit node[:diamond][:collectors][:NetworkCollector][:byte_unit]
+  if node[:diamond][:collectors][:NetworkCollector][:interval]
+    interval node[:diamond][:collectors][:NetworkCollector][:interval]
+  end
 end
 
