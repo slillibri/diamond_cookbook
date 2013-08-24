de5bc7c5e7ac96fc21c37ae383f4d492cff7fbde
Scott Lillibridge <slillibridge@whitepages.com>
2013-08-24 14:20:10 -0700

    1 minute default interval and update ubuntu package version

--- attributes/default.rb
+++ attributes/default.rb
@@ -2,11 +2,11 @@ default["diamond"]["install_method"] = "package"
 default["diamond"]["graphite_server_role"] = nil
 default["diamond"]["graphite_server"] = "graphite"
 default["diamond"]["path_prefix"] = "servers"
-default["diamond"]["interval"] = "300"
+default["diamond"]["interval"] = "60"
 default["diamond"]["handlers"] = "diamond.handler.graphite.GraphiteHandler, diamond.handler.archive.ArchiveHandler"
 case node[:platform]
   when "ubuntu","debian"
-    default[:diamond][:version] = '3.0.2'
+    default[:diamond][:version] = '3.3.568'
   else
     default[:diamond][:version] = '3.0.2-0'
 end
