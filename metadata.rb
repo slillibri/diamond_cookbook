90c0ca188bd53c6021f024b184fde73ce05c9fcf
Scott Lillibridge <slillibridge@whitepages.com>
2013-09-05 15:07:56 -0700

    Version bump

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email "scott@jpopidol.com"
 license          "All rights reserved"
 description      "Installs/Configures diamond"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "1.0.19"
+version          "1.0.20"
 
 supports         'ubuntu'
 supports         'centos'
